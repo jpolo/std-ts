c5895815d633ba1fa593c188a93aac15778995a1
Julien Polo <julien.polo@sportaculous.com>
2015-02-12 15:37:53 +0100

    Added semicolons

--- src/main/typescript/ts/vm.ts
+++ src/main/typescript/ts/vm.ts
@@ -1,46 +1,46 @@
 module vm {
-  var __empty = {}
-  var __global: Window = (new Function("return this;")).call(null)
+  var __empty = {};
+  var __global: Window = (new Function("return this;")).call(null);
 
   export interface IOption {
     sourceURL?: string
     sourceMappingURL?: string
   }
 
-  export var global = __global
+  export var global = __global;
 
   export function compile(jscode: string, options?: IOption): (context?: { [key: string]: any }) => any {
-    options = options || __empty
-    var fnWithContext: Function
-    var fnNoContext: Function
-    var sourceURL = options.sourceURL
-    var sourceMappingURL = options.sourceMappingURL
+    options = options || __empty;
+    var fnWithContext: Function;
+    var fnNoContext: Function;
+    var sourceURL = options.sourceURL;
+    var sourceMappingURL = options.sourceMappingURL;
     
     if (sourceURL) {
       //Firebug and Webkit annotation
-      jscode += "\n//# sourceURL=" + sourceURL
+      jscode += "\n//# sourceURL=" + sourceURL;
     }
     
     if (sourceURL) {
-      jscode += "\n//# sourceMappingURL=" + sourceMappingURL
+      jscode += "\n//# sourceMappingURL=" + sourceMappingURL;
     }
     
     return function (context) {
-      var returnValue
+      var returnValue;
       if (context) {
-        fnWithContext = fnWithContext || new Function("__context__", "with(__context__) {" + jscode + "}")
-        returnValue = fnWithContext.call(context, context)
+        fnWithContext = fnWithContext || new Function("__context__", "with(__context__) {" + jscode + "}");
+        returnValue = fnWithContext.call(context, context);
       } else {
-        fnNoContext = fnNoContext || new Function(jscode)
-        returnValue = fnNoContext()
+        fnNoContext = fnNoContext || new Function(jscode);
+        returnValue = fnNoContext();
       }
-      return returnValue
+      return returnValue;
     }
   }
 
   export function eval(jscode: string, context?: { [key: string]: any }, options?: IOption): any {
-    return compile(jscode, options)(context)
+    return compile(jscode, options)(context);
   }
 }
 
-export = vm
+export = vm;
