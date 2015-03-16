13a9ef3d10d2c82752f30a8895d09ed33aed2614
Julien Polo <julien.polo@sportaculous.com>
2015-03-16 18:47:29 +0100

    stacktrace more robust tests

--- src/test/typescript/ts/stacktrace.test.ts
+++ src/test/typescript/ts/stacktrace.test.ts
@@ -6,12 +6,26 @@ import ICallSite = stacktrace.ICallSite;
 
 var stacktraceSuite = suite("ts/stacktrace", (self) => {
   var FILENAME = "stacktrace.test.js";
-  
-  self.setUp = () => {}
+  var DATA = {
+    ref: <Error> null,
+    callstack: <ICallSite[]> null
+  };
+
+  //var callstack: ICallSite[];
+
+  self.setUp = () => {
+    
+    (new Function(
+      "stacktrace", "exports",
+      //pseudo script
+      "exports.ref = new Error();\n" + 
+      "exports.callstack = stacktrace.create();\n"
+    ))(stacktrace, DATA)
+  }
   
   test("get()", (assert) => {
   
-    var callstack = stacktrace.get(new Error())
+    var callstack = stacktrace.get(DATA.ref)
     var callsite = callstack[0]
     
     assert.ok(Array.isArray(callstack))
@@ -26,8 +40,7 @@ var stacktraceSuite = suite("ts/stacktrace", (self) => {
   
   
   test("create()", (assert) => {
-  
-    var callstack = stacktrace.create()
+    var callstack = DATA.callstack;
     var callsite = callstack[0]
     
     assert.ok(Array.isArray(callstack))
