19b50cd997d5394cfa4bfe5e98fb784fb017e18d
Julien Polo <julien.polo@sportaculous.com>
2015-04-14 14:45:51 +0200

    clone test placeholder

---
+++ src/test/typescript/ts/clone.test.ts
@@ -0,0 +1,10 @@
+import unit = require("../../../main/typescript/ts/unit")
+import test = unit.test
+import clone = require("../../../main/typescript/ts/clone")
+
+var cloneSuite = unit.suite("ts/clone", (self) => {
+  
+})
+
+var exportSuite = cloneSuite
+export = exportSuite
