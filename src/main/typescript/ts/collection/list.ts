53c9197071b0f4baec4dea3319b32556d393b308
jpolo <julien.polo@gmail.com>
2015-06-09 23:51:04 +0200

    list draft

--- src/main/typescript/ts/collection/list.ts
+++ src/main/typescript/ts/collection/list.ts
@@ -7,39 +7,81 @@ module list {
   }
 
   
-  interface IListNode<T> {
+  interface INode<T> {
     value: T
-    previous: IListNode<T>
-    next: IListNode<T>
+    previous: INode<T>
+    next: INode<T>
   }
   
-  var __node = function <T>(v: T, previous?: IListNode<T>, next?: IListNode<T>): IListNode<T> {
-    var node = {
+  var __node = function <T>(v: T): INode<T> {
+    return {
       value: v,
-      previous: previous,
-      next: next
+      previous: null,
+      next: null
     };
-    if (next) {
-      next.previous = node;
-    }
-    if (previous) {
-      previous.next = node;
-    }
-    return node
+  };
+  var __nodeConnect = function <T>(l: INode<T>, r: INode<T>) {
+    l.next = r;
+    r.previous = l;
   };
 
   class List<T> {
     
+    //static isList() {}
+    
     protected _length: number = 0;
-    protected _head: IListNode<T>;
+    protected _head: INode<T>;
     
     constructor() {
       
     }
     
-    push(v: T) {
+    push(v: T): void {
+      var head = this._head;
+      var node = __node(v);
       
+      if (!head) {
+        head = this._head = node.previous = node.next = node;  
+      } else {
+        __nodeConnect(head.previous, node);
+        __nodeConnect(node, head);
+      }
+      this._length += 1;
     }
+    
+    pop(): T {
+      var returnValue: T;
+      if (this._length > 0) {
+        
+        this._length -= 1;
+      }
+      return returnValue;
+    }
+    
+    shift(): T {
+      var returnValue: T;
+      if (this._length > 0) {
+        
+        this._length -= 1;
+      }
+      return returnValue;
+    }
+    
+    unshift(v: T) {
+      var head = this._head;
+      var node = __node(v);
+      
+      if (!head) {
+        head = this._head = node.previous = node.next = node;  
+      } else {
+        __nodeConnect(node, head.next);
+        __nodeConnect(node, head);
+      }
+      this._length += 1;
+    }
+    
+    
+    
     
     
   }
